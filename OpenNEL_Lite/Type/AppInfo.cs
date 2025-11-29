3fb892cd9f3bfc61734a7d7a59b94cb1d68fb991
FandMC <winme2002@outlook.com>
2025-11-29 15:29:08 +0800

    1.0.1 更友好的文字，更多的类型

--- OpenNEL_Lite/Type/AppInfo.cs
+++ OpenNEL_Lite/Type/AppInfo.cs
@@ -1,7 +1,7 @@
 ﻿namespace OpenNEL_Lite.type;
 class AppInfo
 {
-    public const string AppVersion = "1.0.0";
+    public const string AppVersion = "1.0.1";
 
     public const string GithubUrL = "https://github.com/FandMC/OpenNEL_Lite";
     
